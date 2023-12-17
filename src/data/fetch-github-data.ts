620db1100b2183da2c79293d6108af94f3c21fc6
Matheus Kruger <ms-kruger@hotmail.com>
2023-12-17 12:06:29 -0300

    Arrumando alguns espaçamentos e css

--- src/data/fetch-github-data.ts
+++ src/data/fetch-github-data.ts
@@ -2,13 +2,13 @@ import { type User } from "../types/user";
 import { fetchFirebaseData } from "./fetch-firebase-data";
 
 export async function fetchGithubData() {
-  const user = await fetch("https://api.github.com/users/mathkruger");
+  const request = await fetch("https://api.github.com/requests/mathkruger");
 
-  if (user.status === 403) {
+  if (request.status === 403) {
     const fallback = await fetchFirebaseData<User>("fallbackUserData");
     return fallback[0];
   }
 
-  const userJson = await user.json();
-  return userJson as User;
+  const user = await request.json();
+  return user as User;
 }
